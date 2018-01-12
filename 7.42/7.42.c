18540a123e3cfefda335d3b5e072a340f44865b7
AKrupaa <arkadiusz.krupinski15@gmail.com>
2018-01-12 08:44:27 +0100

    s

--- 7.42/7.42.c
+++ 7.42/7.42.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#define MAX 1000
 
-int count(char tab[], char c);
+//liczbe wystapien pierwszego znaku z drugiego ciagu w pierwszym ciagu,
+int count(char tab[], char c)
+{
+	
+}
 int count_string(char tab[], char tab2[]);
 int count_all_of(char tab[], char tab2[], int size 2);
+
+int main()
+{
+	int tab1[MAX];
+	int tab2[MAX];
+	int a=0;
+	int b=0;
+	int liczba;
+	
+	scanf("%i",&liczba);
+	if(liczba!=0)
+	tab1[a++]=liczba;
+	while(liczba!=0;a<MAX)
+	{
+		scanf("%i",&liczba);
+		if(liczba!=0)
+		tab
+	}
+	
+	return 0;
+}

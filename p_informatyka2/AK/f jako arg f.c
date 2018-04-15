43b4a4968cdde4227b3c5f9fb4beeda6cd43e2d1
AKrupaa <33357370+AKrupaa@users.noreply.github.com>
2018-04-15 16:48:46 +0200

    Function as argument of another function
    let's go

---
+++ p_informatyka2/AK/f jako arg f.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float square(float a, float b) {
+        return ((a) * (b));
+}
+
+typedef float( * ActionT )( float, float );
+
+float volume1( float *a, float *b, float *h, ActionT pAction)  {
+        return  (*pAction) ( *a, *b ) * (*h);
+}
+
+float volume_without_typedef( float *a, float *b, float *h, float( * pAction )( float, float ) ) {
+        return ( pAction( *a, *b ) * *h );
+}
+
+int main() {
+        float a,b,h;
+        float *wa, *wb, *wh;
+        printf("Give values: a, b, h(height):\n");
+        scanf("%f %f %f", &a,&b,&h);
+        wa=&a;
+        wb=&b;
+        wh=&h;
+        printf("Volume = %f\n", volume1( wa, wb, wh, square ));
+        printf("Volume without typedef = %f\n", volume_without_typedef( wa, wb, wh, square ));
+        return 0;
+}

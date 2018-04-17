b36d1732f6186e9b48e5f7ce47b489e0e17eaf79
AKrupaa <33357370+AKrupaa@users.noreply.github.com>
2018-04-17 12:35:09 +0200

    1
    1

--- p_informatyka2/AK/f jako arg f.c
+++ p_informatyka2/AK/f jako arg f.c
@@ -4,11 +4,8 @@ float square(float a, float b) {
         return ((a) * (b));
 }
 
-typedef float( * ActionT )( float, float );
 
-float volume1( float *a, float *b, float *h, ActionT pAction)  {
-        return  (*pAction) ( *a, *b ) * (*h);
-}
+
 
 float volume_without_typedef( float *a, float *b, float *h, float( * pAction )( float, float ) ) {
         return ( pAction( *a, *b ) * *h );
@@ -22,7 +19,7 @@ int main() {
         wa=&a;
         wb=&b;
         wh=&h;
-        printf("Volume = %f\n", volume1( wa, wb, wh, square ));
+        //printf("Volume = %f\n", volume1( wa, wb, wh, square ));
         printf("Volume without typedef = %f\n", volume_without_typedef( wa, wb, wh, square ));
         return 0;
 }

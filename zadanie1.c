fc66f5db6d142d0a179a1675923841cd4d83c8fe
AKrupaa <33357370+AKrupaa@users.noreply.github.com>
2018-05-22 11:47:10 +0200

    zadanka ze strukturami
    111

---
+++ zadanie1.c
@@ -0,0 +1,9 @@
+C:\Users\student\Desktop\zadanie1.c	In function 'main':
+80	16	C:\Users\student\Desktop\zadanie1.c	[Warning] passing argument 1 of 'wprowadz_dane' from incompatible pointer type
+13	6	C:\Users\student\Desktop\zadanie1.c	[Note] expected 'struct dane *' but argument is of type 'struct dane **'
+81	14	C:\Users\student\Desktop\zadanie1.c	[Warning] passing argument 1 of 'drukuj_dane' from incompatible pointer type
+28	6	C:\Users\student\Desktop\zadanie1.c	[Note] expected 'struct dane *' but argument is of type 'struct dane **'
+82	18	C:\Users\student\Desktop\zadanie1.c	[Warning] passing argument 1 of 'zapisz_do_pliku' from incompatible pointer type
+34	6	C:\Users\student\Desktop\zadanie1.c	[Note] expected 'struct dane *' but argument is of type 'struct dane **'
+83	18	C:\Users\student\Desktop\zadanie1.c	[Warning] passing argument 1 of 'wczytaj_z_pliku' from incompatible pointer type
+47	6	C:\Users\student\Desktop\zadanie1.c	[Note] expected 'struct dane *' but argument is of type 'struct dane **'
